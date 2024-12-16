be8341b75e109aca5ec42643d3338a9963539300
Bereket Amha <amha.bereket@gmail.com>
2024-12-16 23:35:30 +0300

    REPL coded

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
   "strings"
+	"bufio"
+	"os"
+	"log"
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	for {
+	fmt.Print("Pokedex > ")
+	scanner := bufio.NewScanner(os.Stdin)
+  scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := scanner.Text()
+	words := strings.Fields(strings.ToLower(text))
+	fmt.Printf("Your command was: %v\n", words[0])
+
+}
 }
 
 
@@ -16,4 +31,7 @@ func CleanInput(text string) []string {
 
 	return words  
 }
+
+
+
 
